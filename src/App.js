122581dfb8e02004bb559a58a1f1820b2f9b5604
unknown <gabrielayoku@gmail.com>
2022-10-29 22:47:37 +0100

    fixing title attribute

--- src/App.js
+++ src/App.js
@@ -15,27 +15,23 @@ function App() {
      <header>
       <center><img src={profile} alt="" id='profile_img'/></center>
       <p id='twitter'>@AlPHA_MHALE</p>
-      <span className='dummy'>haryor</span>
+      {/* /*<span className='dummy'>haryor</span> */}
       { windowx >'500'?<img className='arrow' src={arrow} alt="" />:<img className='arrow' alt='' src={three}/>}
      </header>
      <main>
       <a className='link' href="https://twitter.com/alphamhale/">Twitter Link</a>
       <a className='link' id='btn_zuri' href="https://training.zuri.team/">Zuri Team</a>
-      <a id='books' href="http://books.zuri.team">
+      <a title='Get the best books on design and coding with illustration and code snippet for better comprehension' className='link' id='books' href="http://books.zuri.team">
         <span>Zuri Books</span>
-        <span>Get the best books on design and coding with illustration and code snippet for better comprehension</span>
       </a>
-      <a id='book_python' href="https://books.zuri.team/python-for-beginners?ref_id=<haryor>">
+      <a title='All you need to become a better programmer in just one book' className='link' id='book_python' href="https://books.zuri.team/python-for-beginners?ref_id=<haryor>">
         <span>Python Books </span>
-        <span>All you need to become a better programmer in just one book</span>
       </a>
-      <a id='pitch' href="https://background.zuri.team">
+      <a  title='Let our team help you run background checks on your codes' className='link' id='pitch' href="https://background.zuri.team">
         <span>Background  Check for coders</span>
-        <span>Let our team help you run background checks on your codes</span>
       </a>
-      <a id='book_design' href="https://books.zuri.team/design-rules">
+      <a title='Get Your Hands on the free design book offered by our team' className='link' id='book_design' href="https://books.zuri.team/design-rules">
         <span> Design Books</span>
-        <span>Get Your Hands on the free design book offered by our team</span>
       </a>
       <aside>
         <img src={slack} alt="" />
